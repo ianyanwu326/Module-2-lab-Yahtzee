9947f932a84260a301d7aca3f6607ead672597d6
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-12 20:03:58 -0500

    I Created ConsoleUI class with display and input methods.

--- ConsoleUI.h
+++ ConsoleUI.h
@@ -1,1 +1,13 @@
-#pragma once
+#ifndef CONSOLE_UI_H
+#define CONSOLE_UI_H
+
+#include <vector>
+#include <string>
+
+class ConsoleUI {
+public:
+  void displayWelcome() const;
+  void displayDice(const std::vector<int>& dice, const std::vector<bool>& heldStatus) const;
+  void displayScoreOptions(const std::vector<std::string>& options, const std::vector<int>& scores) const;
+
+  
