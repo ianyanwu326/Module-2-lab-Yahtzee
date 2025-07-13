abd787ec5f36898b8acef4d2e0890720bec8b400
ikenna Anyanwu <ianyanwu@cub.uca.edu>
2025-07-12 21:35:02 -0500

    I defined GameLogic class with dice, score, and category tracking.

--- GameLogic.h
+++ GameLogic.h
@@ -1,1 +1,17 @@
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+#include <vector>
+#include <string>
+
+class GameLogic {
+private:
+  std::vector<Die> dice;
+  int rollsRemaining;
+  std::vector<int> scores;
+  std::vector<bool> categoriesUsed;
+
+  void rollAllUnheldDice();
+  int calculateScoreForCategory(int category) const;
+  bool isCategoryUsed(int category) const;
 
